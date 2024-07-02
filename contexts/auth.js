67529c99c7589c0d59195f5f57138bdaf2f6be19
Nick Swinford <NicholasJohn16@gmail.com>
2024-07-01 23:53:56 -0400

    improve auth workflow

--- contexts/auth.js
+++ contexts/auth.js
@@ -1,33 +1,72 @@
-import React from 'react';
-import { createSession, getSession, deleteSession } from '../utils/Anahita';
+import { createContext, useContext, useEffect, useState} from 'react';
+import { createSession, getSession, deleteSession } from '../api/people';
 
 // heavily inspired from https://docs.expo.dev/router/reference/authentication/
 
-const signIn = (username, password) => {
-
-}
-
-const AuthContext = React.createContext({
-    signIn: createSession,
-    signOut: deleteSession,
-    currentUser: getSession,
-    session: null,
+const AuthContext = createContext({
+    signIn: () => {},
+    signOut: () => {},
+    getSession: () => {},
+    currentUser: null,
     isLoading: false
 });
 
 export function useSession() {
-    const value = React.useContext(AuthContext);
+    const value = useContext(AuthContext);
 
     return value;
 }
 
 export function SessionProvider({children}) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    const setupCurrentUser = async () => {
+        try {
+            const session = await getSession();
+            setCurrentUser(session);
+        } catch (error) {
+            setCurrentUser(null);
+        }
+        
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        setupCurrentUser();
+    }, []);
+
+    const signIn = async (username, password) => {
+        setLoading(true);
+
+        try {
+            const session = await createSession(username, password)
+            setCurrentUser(session);
+            return session;
+        } catch (error) {
+            setCurrentUser(null);
+            return false;
+        } finally {
+            setLoading(false);
+        }             
+    }
+
+    const signOut = () => {
+        setLoading(true);
+        console.log('auth.signOut');
+        deleteSession().then((result) => { 
+            console.log(result);
+            setLoading(false);
+            setCurrentUser(null);
+        });
+    }
+
     const value = {
-        signIn: createSession,
-        signOut: deleteSession,
-        currentUser: getSession,
-        session: null,
-        isLoading: false
+        signIn,
+        signOut,
+        getSession,
+        currentUser,
+        isLoading
     }
 
     return (
