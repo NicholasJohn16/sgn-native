48b688c842fe05926a0c4b611502f440e8423eb1
Nick Swinford <NicholasJohn16@gmail.com>
2024-07-29 15:43:07 -0400

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,13 @@
+import { StorybookConfig } from '@storybook/react-native';
+
+const main: StorybookConfig = {
+  // stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
+  stories: ['../components/**/*.stories.?(ts|tsx|js|jsx)'],
+  // stories: ['../components/ui/**/*.stories.?(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-actions',
+  ],
+};
+
+export default main;
