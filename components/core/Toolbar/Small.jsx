935ecd2ae3b1257dd72aaaa96ba91dfc009251c9
Nick Swinford <NicholasJohn16@gmail.com>
2024-08-09 03:02:44 -0400

    add delete function to small toolbar

--- components/core/Toolbar/Small.jsx
+++ components/core/Toolbar/Small.jsx
@@ -2,14 +2,26 @@ import { HStack } from '@/components/ui/hstack';
 import { Text } from '@/components/ui/text';
 import { Humanize } from '@/components/helpers/date/humanize';
 import { Button, ButtonText } from '@/components/ui/button';
+import { Link, router } from 'expo-router';
+import { getNamespace } from '@/utils/node';
+import { request } from '@/api/api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export function Small({item, showTime = false, className}) {
+    const queryClient = useQueryClient();
+    const { app, entity } = getNamespace(item);
 
-    console.log(item, 'item');
+    const hasCommand = (name) => item?.commands && item.commands.includes(name);
+    const handleDelete = async () => await request(`/${app}/${item.id}`, {}, 'DELETE');
 
-    const hasCommand = (name) => {
-        return item?.commands && item.commands.includes(name);
-    }
+    const mutation = useMutation({
+        mutationFn: () => handleDelete(),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [entity, {id: item.id.toString()}] });
+            queryClient.invalidateQueries({ queryKey: ['stories'] });
+            queryClient.invalidateQueries({ queryKey: [app] });
+        }
+    });
 
     return (
         <HStack space="sm" className={`${className}`}>
@@ -27,12 +39,14 @@ export function Small({item, showTime = false, className}) {
 
             {hasCommand('edit') && 
                 <Button size="xs" variant="link" action="secondary">
-                    <ButtonText>Edit</ButtonText>
+                    <ButtonText>
+                        <Link href={`/notes/${item.id}/edit`}>Edit</Link>
+                    </ButtonText>
                 </Button>
             }
 
             {(hasCommand('delete') || hasCommand('deletecomment')) && 
-                <Button size="xs" variant="link" action="secondary">
+                <Button onPress={() => mutation.mutate()} size="xs" variant="link" action="secondary">
                     <ButtonText>Delete</ButtonText>
                 </Button>
             }
