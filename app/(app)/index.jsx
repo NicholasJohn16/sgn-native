2b75fa06f17948cb89c1a3e47b76f0755da3b348
Nick Swinford <NicholasJohn16@gmail.com>
2024-08-09 03:06:10 -0400

    move dashboard to index page

--- app/(app)/index.jsx
+++ app/(app)/index.jsx
@@ -1,11 +1,22 @@
-import Dashboard from './dashboard';
-import { Text } from 'react-native';
-
-console.log(Dashboard);
+import { getLeaderStories } from "@/api/stories";
+import { StoryLoader } from "@/components/core/story/loader";
+import { InfiniteScroll } from "@/components/core/InfiniteScroll";
+import { Skeletons, SkeletonBase } from "@/components/core/story/Skeleton";
+import { Composer } from '@/components/core/Composer';
 
 export default function Index() {
+    const getStories = async (params) => {
+        const response = await getLeaderStories({start: params.pageParam});
+        return response.data;
+    }
+
     return (
-        // <Text>Index</Text>
-        <Dashboard />
+        <InfiniteScroll 
+            headerComponent={Composer}
+            queryKey={['stories', { filter: 'leaders'}]}
+            fetchItems={getStories}
+            renderItem={({item}) => <StoryLoader story={item} key={item.id} />}
+            skeleton={Skeletons}
+        />
     )
 }
