c8513b99251353a3a507f5feee42ab371099bc20
Nick Swinford <NicholasJohn16@gmail.com>
2024-08-09 02:09:51 -0400

    fix spelling error

--- app/(app)/notes/index.jsx
+++ app/(app)/notes/index.jsx
@@ -1,6 +1,6 @@
 import { getNotes } from '@/api/notes';
 import { List as Note } from '@/features/notes/components/List';
-import { InfiniteScroll } from '@/components/core/InfintieScroll';
+import { InfiniteScroll } from '@/components/core/InfiniteScroll';
 import { Skeletons } from '@/features/notes/components/Skeleton';
 
 export default function Index() {
